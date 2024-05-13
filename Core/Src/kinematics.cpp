630a06849516970f1bab5d6188a0a8f2ef21515a
Suyog Bhandari <079bct091.suyog@pcampus.edu.np>
2024-05-13 21:31:07 +0545

    more in kinematics

--- Core/Src/kinematics.cpp
+++ Core/Src/kinematics.cpp
@@ -1,22 +1,29 @@
 #include "joystick.hpp"
 
-class kinematics:
-{
+class kinematics{
 	private:
 		float motor_omegas[4];
 		double inv_matrix[4][3];
+		float wheel_radius;
+		float base_radius;
 	public:
 		float get_motor_omegas()
 		{
 			float twist[3];
-			twist[0] = joystick.get_twist(0);
-			twist[1] = joystick.get_twist(1);
-			twist[2] = joystick.get_twist(2);
+			twist[0] = joystick.get_twist(0); // get Vx
+			twist[1] = joystick.get_twist(1); // get Vy
+			twist[2] = joystick.get_twist(2) * base_radius; // get omega
 
-			for (i=0;i<4;i++) {
-				motor_omegas[0] 			
+			for (int i=0;i<4;i++) {
+				for(int j=0;j<3;j++)
+				{
+					motor_omegas[i] += inv_matrix[i][j] * twist[j]; 
+				}
+				motor_omegas[i] /= wheel_radius;
 			}
+
+			return *motor_omegas;
 		}
-}   
+};   
 
 
