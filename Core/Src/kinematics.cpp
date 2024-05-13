261b3b612e3cec1be12b76201bd5009f54f1244d
ujwacha <ujwolacharya4@gmail.com>
2024-05-14 01:02:35 +0545

    Filled up the calculated inverse kainematics matrix from octave

--- Core/Src/kinematics.cpp
+++ Core/Src/kinematics.cpp
@@ -1,29 +1,53 @@
 #include "joystick.hpp"
 
-class kinematics{
-	private:
-		float motor_omegas[4];
-		double inv_matrix[4][3];
-		float wheel_radius;
-		float base_radius;
-	public:
-		float get_motor_omegas()
-		{
-			float twist[3];
-			twist[0] = joystick.get_twist(0); // get Vx
-			twist[1] = joystick.get_twist(1); // get Vy
-			twist[2] = joystick.get_twist(2) * base_radius; // get omega
+class Kinematics{
+private:
+  float motor_omegas[4];
+  double inv_matrix[4][3];
+  float wheel_radius;
+  float base_radius;
+public:
+  
+  Kinematics() {
+    // fill up the kinematics matrix
+    inv_matrix[0][0] = -0.3535533905932738;
+    inv_matrix[0][1] = 0.3535533905932737;
+    inv_matrix[0][2] = 0.25;
 
-			for (int i=0;i<4;i++) {
-				for(int j=0;j<3;j++)
-				{
-					motor_omegas[i] += inv_matrix[i][j] * twist[j]; 
-				}
-				motor_omegas[i] /= wheel_radius;
-			}
+    inv_matrix[1][0] = 0.3535533905932738;
+    inv_matrix[1][1] = 0.3535533905932738;
+    inv_matrix[1][2] = -0.25;
 
-			return *motor_omegas;
-		}
+    inv_matrix[2][0] = -0.3535533905932738;
+    inv_matrix[2][1] = 0.3535533905932738;
+    inv_matrix[2][2] = -0.25;
+
+    inv_matrix[3][0] = 0.3535533905932738;
+    inv_matrix[3][1] = 0.3535533905932738;
+    inv_matrix[3][2] = 0.25;
+   
+
+
+  }
+  // where is the fucking constructor
+    float get_motor_omegas()
+  {
+    float twist[3];
+    twist[0] = joystick.get_twist(0); // get Vx
+    twist[1] = joystick.get_twist(1); // get Vy
+    twist[2] = joystick.get_twist(2) * base_radius; // get omega
+    
+    for (int i=0;i<4;i++) {
+      for(int j=0;j<3;j++)
+	{
+	  motor_omegas[i] += inv_matrix[i][j] * twist[j]; 
+	}
+      motor_omegas[i] /= wheel_radius;
+    }
+    
+    return *motor_omegas;
+  }
+  
 };   
 
 
