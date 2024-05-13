86deaf061fc3de140da359b5cb61e93661cc7aee
Suyog Bhandari <079bct091.suyog@pcampus.edu.np>
2024-05-13 21:23:51 +0545

    changes in kinematics

--- Core/Src/kinematics.cpp
+++ Core/Src/kinematics.cpp
@@ -1,7 +1,22 @@
 #include "joystick.hpp"
 
-class kinematics :
-    public :
-    
+class kinematics:
+{
+	private:
+		float motor_omegas[4];
+		double inv_matrix[4][3];
+	public:
+		float get_motor_omegas()
+		{
+			float twist[3];
+			twist[0] = joystick.get_twist(0);
+			twist[1] = joystick.get_twist(1);
+			twist[2] = joystick.get_twist(2);
+
+			for (i=0;i<4;i++) {
+				motor_omegas[0] 			
+			}
+		}
+}   
 
 
